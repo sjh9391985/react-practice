9b065337000b07497f5b4283ec4d03fc0f93e016
JaeHyeon Son <sjh9391985@gmail.com>
2021-07-23 09:55:43 +0900

    babel#1

---
+++ basics/ex06/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = function(){
+    const message = 'Hello World'
+    return (
+        <div>
+            <h1>{message}</h1>
+        </div>
+    )
+}
+
+export default App;
---
+++ basics/ex06/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import  ReactDOM from 'react-dom';
+import App from './App'; // {} <- 안의 이름이 정확하게 맞아야 한다.
+
+ReactDOM.render(<App/>, document.getElementById('root'));
