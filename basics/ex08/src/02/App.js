04247c746a73533f6128f8d3fb282ecfc218a824
JaeHyeon Son <sjh9391985@gmail.com>
2021-07-26 11:55:52 +0900

    jsx tutorials#3

---
+++ basics/ex08/src/02/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function(){
+    /*
+        리액트 컴포넌트는 단일 루트 노드만 랜더링 할 수 있다.
+
+    */
+    return (
+        <div id="App">
+            <h2>App02</h2>
+            <p>JSX Tutorials - 특징1: HTML과의 차이점</p>
+        </div>
+    );
+}
