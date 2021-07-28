716187f7a3311b6a7be362fe119c0faf112556ce
JaeHyeon Son <sjh9391985@gmail.com>
2021-07-28 09:54:04 +0900

    kanban-board#1

---
+++ component/kanban/src/ex01/App.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+
+export default class extends Component{
+    render() {
+        return (
+            <div id='App'>  {/* kanban board */}
+asdasdasas
+            </div>
+        );
+    }
+}
