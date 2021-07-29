4b8680f2f3384d982dc02c79ec8c1865ae7404f5
JaeHyeon Son <sjh9391985@gmail.com>
2021-07-29 12:38:27 +0900

    component styling #3

---
+++ component/ex02/src/04/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './assets/css/App.css';
+import Banner01 from './Banner01';
+import Banner02 from './Banner02';
+
+export default function App(){
+    
+    return (
+        <div id='App'>
+            <Banner01 />
+            <Banner02 />
+        </div>
+    );
+    
+}
