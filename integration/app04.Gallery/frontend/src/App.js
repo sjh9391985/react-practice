895e8a9dddb1bf2d68b05b2f403b24f320247d3a
JaeHyeon Son <sjh9391985@gmail.com>
2021-08-17 10:53:41 +0900

    router#1

---
+++ integration/app04.Gallery/frontend/src/App.js
@@ -0,0 +1,48 @@
+import React, { Fragment, useState, useRef } from 'react';
+import Modal from "react-modal";
+import styles from './assets/scss/modal.scss'
+Modal.setAppElement('body');
+
+export default function App() {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+
+
+    return (
+
+        <Fragment>
+            <h1>My Photos</h1>
+            <button onClick={ () => setModalIsOpen(true) }>이미지 올리기</button>
+            
+
+
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={ () => setModalIsOpen(false) }
+                shouldCloseOnOverlayClick={ true }
+                className={ styles.Modal }
+                overlayClassName={ styles.Overlay }
+                style={ {content: {width: 350}} }
+                contentLabel="modal05 example">
+                <h1>비밀번호입력</h1>
+                <div>
+                    <form>
+                        <label>코멘트</label>
+                        <br/>
+                        <input type='text'/>
+                        <br/><br/>
+                        <label>이미지</label>
+                        <br/>
+                        <input type='file' />
+                    </form>
+                </div>
+                <div className={ styles['modal-dialog-buttons'] }>
+                    <button>확인</button>
+                    <button onClick={ () => setModalIsOpen(false) }>취소</button>
+                </div>
+            </Modal>
+
+
+        </Fragment>
+    );
+}
---
+++ integration/app04.Gallery/frontend/src/Image.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+export default function Image() {
+
+    return (
+        <ul>
+            <li>
+                <span style="background-image: url('./gallery-examples/im1.jpg')"></span>
+                <a href="">삭제</a>
+            </li>
+            <li>
+                <span style="background-image: url('./gallery-examples/im2.jpg')"></span>
+                <a href="">삭제</a>
+            </li>
+            <li>
+                <span style="background-image: url('./gallery-examples/im3.jpg')"></span>
+                <a href="">삭제</a>
+            </li>
+            <li>
+                <span style="background-image: url('./gallery-examples/im4.jpg')"></span>
+                <a href="">삭제</a>
+            </li>
+            <li>
+                <span style="background-image: url('./gallery-examples/im5.jpg')"></span>
+                <a href="">삭제</a>
+            </li>
+            <li>
+                <span style="background-image: url('./gallery-examples/im6.jpg')"></span>
+                <a href="">삭제</a>
+            </li>
+            <li>
+                <span style="background-image: url('./gallery-examples/im7.jpg')"></span>
+                <a href="">삭제</a>
+            </li>
+            <li>
+                <span style="background-image: url('./gallery-examples/im8.jpg')"></span>
+                <a href="">삭제</a>
+            </li>
+            <li>
+                <span style="background-image: url('./gallery-examples/im9.jpg')"></span>
+                <a href="">삭제</a>
+            </li>
+            <li>
+                <span style="background-image: url('./gallery-examples/im10.jpg')"></span>
+                <a href="">삭제</a>
+            </li>
+            <li>
+                <span style="background-image: url('./gallery-examples/im11.jpg')"></span>
+                <a href="">삭제</a>
+            </li>
+            <li>
+                <span style="background-image: url('./gallery-examples/im12.jpg')"></span>
+                <a href="">삭제</a>
+            </li>
+        </ul>
+
+    )
+
+}
